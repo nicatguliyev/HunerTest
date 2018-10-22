2b0b9bfb322a33679b694af406abe9006c1566e0
*Nicat <nicatguliyev3@gmail.com>
2018-10-22 11:31:00 +0300

    Grid Yaradildi

--- app/src/main/java/com/example/artservis/hunertest/SelectDate.java
+++ app/src/main/java/com/example/artservis/hunertest/SelectDate.java
@@ -48,14 +48,15 @@ public class SelectDate extends AppCompatActivity {
 
         ActionBar mActionBar = getSupportActionBar();
 
-        mActionBar.setDisplayShowHomeEnabled(false);
-        mActionBar.setDisplayShowTitleEnabled(false);
-        mActionBar.setDisplayShowCustomEnabled(true);
-        mActionBar.setElevation(0);
-        // mActionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#616060")));
-        mActionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_actionbar));
-        ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(R.layout.select_date_actionbar, null);
-        mActionBar.setCustomView(actionBarLayout);
+      //  mActionBar.setDisplayShowHomeEnabled(false);
+      //  mActionBar.setDisplayShowTitleEnabled(false);
+      //  mActionBar.setDisplayShowCustomEnabled(true);
+      //  mActionBar.setElevation(0);
+      //  mActionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_actionbar));
+      //  ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(R.layout.select_date_actionbar, null);
+      //  mActionBar.setCustomView(actionBarLayout);
+
+        mActionBar.hide();
 
 
         months.put("01", "Yanvar");
--- app/src/main/java/com/example/artservis/hunertest/Services.java
+++ app/src/main/java/com/example/artservis/hunertest/Services.java
@@ -1,7 +1,11 @@
 package com.example.artservis.hunertest;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Services extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class Services extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        ActionBar mActionBar = getSupportActionBar();
+
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        mActionBar.setDisplayShowCustomEnabled(true);
+        mActionBar.setElevation(0);
+        mActionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_actionbar));
+        ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(R.layout.select_date_actionbar, null);
+        mActionBar.setCustomView(actionBarLayout);
+
+        ImageView backImage = mActionBar.getCustomView().findViewById(R.id.menuIcon);
+
+        backImage.setImageResource(R.drawable.big_back_arrow);
+
+
     }
 }
